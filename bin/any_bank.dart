6c22236b122531e793836eaa95c66b04eac08a8f
geovannifrnaca <geovannidefranca98@gmail.com>
2025-08-15 15:54:35 -0300

    conta corrente

--- bin/any_bank.dart
+++ bin/any_bank.dart
@@ -3,7 +3,7 @@ import 'package:any_bank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
-  ContaCorrente contaCris = ContaCorrente("Cris", 4500);
+  ContaCorrente contaCris = ContaCorrente("Cris", 500);
   ContaPoupanca contaLucca = ContaPoupanca("Lucca", 500);
 
   List<Conta> contas = [contaRoberta, contaMatheus, contaCris, contaLucca];
@@ -17,8 +17,11 @@ void main() {
   contaMatheus.enviar(350);
 
   contaCris.imprimirSaldo();
-  contaCris.receber(100);
+
 
   contaLucca.receber(250);
   contaLucca.enviar(100);
+
+  contaLucca.calcularRendimento();
+  contaCris.enviar(800);
 }
--- lib/conta.dart
+++ lib/conta.dart
@@ -24,10 +24,36 @@ class Conta {
   }
 }
 
+
+
 class ContaCorrente extends Conta {
+  double cheque = 300;
   ContaCorrente(super.titular, super.saldo);
+  @override
+  void enviar(double valor) {
+    if(_saldo + cheque >= valor){
+      _saldo -= valor;
+      imprimirSaldo();
+    }else{
+      print("Sem saldo");
+      imprimirSaldo();
+    }
+    super.enviar(valor);
+  }
 }
 
 class ContaPoupanca extends Conta {
+  double redimento = 0.05;
   ContaPoupanca(super.titular, super.saldo);
+
+  void calcularRendimento() {
+    _saldo += redimento * _saldo ;
+    print("""
+    Titualar: $titular
+    Saldo: $_saldo
+    """);
+  }
+
+  void imprimirSaldoPoupanca() {}
+
 }
